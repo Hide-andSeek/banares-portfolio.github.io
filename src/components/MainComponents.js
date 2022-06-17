eca440367b9cb4de3344fd10c9ed98b0c24eac01
Hide-andSeek <jade.arinal.banares@gmail.com>
2022-06-18 01:26:30 +0800

    Adjusting HTML Content

--- src/components/MainComponents.js
+++ src/components/MainComponents.js
@@ -1,18 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
+import mainContentImg from "../images/Mobile-Phone.svg"
 
 export default function MainComponents(){
     return(
         <div>
             <section className="main-content">
                 <div>
-                    <h1>I am Jade Bañares</h1>
+                    <h1>Jade Bañares</h1>
                     <p>I have a passion in Web Development</p>
+                    <a href=""><i className="fa"></i>GitHub</a>
+                    <a href=""><i className="fa"></i>LinkedIn</a>
+                    <a href=""><i className="fa"></i>Resume</a>
                 </div>
                 <div>
-                    
                     <h2>Reasons why I love REACT!</h2>
+                    <img src={mainContentImg} />
                     <ol>
                         <li>It is maintainable</li>
                         <li>It is composable</li>
